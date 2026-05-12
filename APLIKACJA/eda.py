fe24efc2697304d0a0c397925c288cc5951a8e86
malwinaniedziolka <mmalwina.niedziolka@gmail.com>
2026-05-12 16:01:10 +0200

    wykres do opisu danych

--- APLIKACJA/eda.py
+++ APLIKACJA/eda.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
 sciezkaDoDanych = os.path.join('..','TopBabyNamesbyState.csv')
 
 df = pd.read_csv(sciezkaDoDanych)
 
-print(df.head())
+#Wykres z liczbą przykładów dla Top 10 imion (odkomentowac jesli chce sie uzyc)
+# df['Top Name'].value_counts().sort_values(ascending=False)[:10].plot(kind='bar')
 
-print(df['Year'].sort_values(ascending=False).head())
+# plt.ylabel('Liczba przykładów')
+# plt.xlabel('')
+# plt.title('Liczba przykładów dla Top 10 imion')
+# plt.show()
